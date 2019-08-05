1812a339a1eb536a2d60dd3fd12b15876062b817
Will <doggy.huang@gmail.com>
2019-08-05 19:12:46 +0800

    示範範本參考變數

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  keyword = 'TEST';
+  keyword = '';
 
   clearKeyword() {
     this.keyword = '';
+  }
+
+  doSearch(keyword: string) {
+    this.keyword = keyword;
+    alert('You searched: ' + keyword);
   }
 }
