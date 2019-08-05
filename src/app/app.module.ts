cea34153df5344ff7f0706ca95e964f2e8b82df4
Will <doggy.huang@gmail.com>
2019-08-05 21:15:14 +0800

    示範 @Input() 的用法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
